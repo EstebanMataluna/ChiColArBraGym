e6588b832312e5fb01d31c067d66c3e897f3f1c6
@EstebanMataluna <estebanmataluna@gmail.com>
2023-11-29 19:07:20 -0300

    Visualizando datos json register

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,18 @@
+import User from '../models/user.model.js'
 
-export const register = (req, res) => res.send('register');
+export const register = (req, res) => {
+    const {username, email, password} = req.body
+
+    console.log(username,email,password)
+
+    const newUser = new User({
+        username,
+        email,
+        password
+    })
+    console.log(newUser)
+
+    res.send('Registrando')
+};
 
 export const login = (req, res) => res.send('login');
